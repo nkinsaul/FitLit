c4c50436633c4b927203abbea34999bcdefef882
Quynh Luu <quynhtlluu@gmail.com>
2022-12-11 20:30:47 -0800

    Change label for Hydration chart

--- src/Chart.js
+++ src/Chart.js
@@ -45,6 +45,45 @@ const hoursSleptOverWeekChart = (userData) => {
   });
 };
 
+const hoursSleptOverWeekChart = (userData) => {
+  const sleepChart = new Chart(myChart, {
+    type: "bar",
+    data: {
+      labels: [
+        `${userData[0].date}`,
+        `${userData[1].date}`,
+        `${userData[2].date}`,
+        `${userData[3].date}`,
+        `${userData[4].date}`,
+        `${userData[5].date}`,
+        `${userData[6].date}`,
+      ],
+      datasets: [
+        {
+          label: "Hours Slept",
+          data: [
+            userData[0].hoursSlept,
+            userData[1].hoursSlept,
+            userData[2].hoursSlept,
+            userData[3].hoursSlept,
+            userData[4].hoursSlept,
+            userData[5].hoursSlept,
+            userData[6].hoursSlept,
+          ],
+          borderWidth: 0.5,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+};
+
 const sleepQualityOverWeekChart = (userData) => {
   const sleepChart = new Chart(mySecondChart, {
     type: "bar",
